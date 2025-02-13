4719692fce849628d2b29a120601a6a14f7bb14e
hs33192 <hs33192@10.50.106.137>
2025-02-13 15:00:15 +0530

    Code added to ilterate hashmap with entryset() and keyset() method

--- JavaProject/src/com/java/practice/HashMapProgram.java
+++ JavaProject/src/com/java/practice/HashMapProgram.java
@@ -1,7 +1,9 @@
 package com.java.practice;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class HashMapProgram {
 
@@ -14,16 +16,40 @@ public class HashMapProgram {
 		map.put("last_name", "Patil");
 
 		System.out.println(map.get("email"));
+		System.out.println("========================================");
 
+		for (String key : map.keySet()) {
 
+			System.out.println(key + " is " + map.get(key));
 
-			for (String key : map.keySet()) {
+		}
+		System.out.println();
 
-				System.out.println(key+ " is " +map.get(key));
+		System.out.println("=================By using KeySet method=======================");
+		System.out.println();
 
+		Iterator<String> it = map.keySet().iterator();
+		while (it.hasNext()) {
 
-			}
+			String key = it.next();
+			String value = map.get(key);
 
+			System.out.println("Key is " + key + " value is " + value);
+		}
+		System.out.println();
+
+		System.out.println("=================By Using entrySet method=======================");
+		System.out.println();
+
+		Iterator<Entry<String, String>> iEntry = map.entrySet().iterator();
+
+		while (iEntry.hasNext()) {
+
+			Entry<String, String> entry = iEntry.next();
+
+			System.out.println("Key is " + entry.getKey() + " Value is " + entry.getValue());
+
+		}
 
 	}
 
